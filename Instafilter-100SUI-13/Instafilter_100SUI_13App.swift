43b94d85dd367af623b42252bffc1e461f854851
Kuncans <akafauna@gmail.com>
2022-03-17 22:10:27 +0000

    Initial Commit

---
+++ Instafilter-100SUI-13/Instafilter_100SUI_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter_100SUI_13App.swift
+//  Instafilter-100SUI-13
+//
+//  Created by Duncan Kent on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter_100SUI_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
