a2003c5c64c5d5af3b2d5ac2376e8430bb92c17f
Kuncans <akafauna@gmail.com>
2022-03-17 22:42:42 +0000

    Add ImagePicker with UIViewControllerRepresentable

--- Instafilter-100SUI-13/ContentView.swift
+++ Instafilter-100SUI-13/ContentView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedImage: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingPickerView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            selectedImage?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingPickerView = true
+            }
+        }
+        .sheet(isPresented: $showingPickerView) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
     }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        selectedImage = Image(uiImage: inputImage)
+
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
